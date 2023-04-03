9b7e17b89d8964b4269ae98f66f0080359621cec
zen <zhangyiming748@gmail.com>
2023-04-03 22:20:25 +0800

    slog格式由调用方设置

--- go.mod
+++ go.mod
@@ -3,10 +3,10 @@ module github.com/zhangyiming748/processImage
 go 1.19
 
 require (
-	github.com/zhangyiming748/GetAllFolder v0.0.7
-	github.com/zhangyiming748/GetFileInfo v0.0.21
+	github.com/zhangyiming748/GetAllFolder v0.0.8
+	github.com/zhangyiming748/GetFileInfo v0.0.22
 	github.com/zhangyiming748/replace v0.0.7
-	github.com/zhangyiming748/voiceAlert v0.0.13
+	github.com/zhangyiming748/voiceAlert v0.0.15
 )
 
 require golang.org/x/exp v0.0.0-20230321023759-10a507213a29
--- unit_test.go
+++ unit_test.go
@@ -1,12 +1,32 @@
 package processImage
 
 import (
+	"golang.org/x/exp/slog"
 	"testing"
 )
 
 func TestProcessImages(t *testing.T) {
-	dir := "/Volumes/swap/Back"
+	//setLog()
+	dir := "/Users/zen/Downloads"
 	pattern := "jpg;JPG;png;PNG;jpeg"
 	threads := "4"
 	ProcessAllImages(dir, pattern, threads)
+	slog.Debug("done!")
 }
+
+//func setLog() {
+//	//var level slog.Level
+//	var opt slog.HandlerOptions
+//	opt = slog.HandlerOptions{ // 自定义option
+//		AddSource: true,
+//		Level:     slog.LevelDebug, // slog 默认日志级别是 info
+//	}
+//	file := "test.log"
+//	logf, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0777)
+//	if err != nil {
+//		panic(err)
+//	}
+//	//defer logf.Close() //如果不关闭可能造成内存泄露
+//	logger := slog.New(opt.NewJSONHandler(io.MultiWriter(logf, os.Stdout)))
+//	slog.SetDefault(logger)
+//}
